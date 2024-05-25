8fb80d8a723a2cdfca08a90d6d6f3ff9bc389d58
SHADOWZXCV <faded.lover.shadows@gmail.com>
2024-05-25 16:10:56 +0300

    Commented out signup, to use only signin, and added vaults schema

--- route/index.js
+++ route/index.js
@@ -1,5 +1,5 @@
 const signInRouter = require('@Route/signin');
-const signupRouter = require('@Route/signup/users');
+// const signupRouter = require('@Route/signup/users');
 const passport = require('passport');
 
 module.exports = (app) => {
